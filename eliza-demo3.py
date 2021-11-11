fde55ff2e7a04e7c4d2528b864a1fcff60f9fe8e
Eliza Climo <eclimo@villanova.edu>
2021-11-11 15:48:20 -0500

    sprint review 3, version 2

--- eliza-demo3.py
+++ eliza-demo3.py
@@ -1,10 +1,15 @@
-﻿import viz
+﻿import viz 
 import vizshape
 import atexit
+import vizinfo
+import viztask
+import vizact
+import vizcam
+
 
 viz.go()
 
-filename = "VUCEET013.jpg"
+filename = "Panoramas/VUCEET013.jpg"
 sphere = vizshape.addSphere(radius=20, pos=[0,0,0])
 this_texture = viz.addTexture(filename)
 
@@ -15,9 +20,12 @@ sphere.disable(viz.LIGHTING) #make the image appear full color, do not render sh
 #Flip normals to make texture go on the inside of sphere
 sphere.enable(viz.FLIP_POLYGON_ORDER)
 
+
 view = viz.MainView
 view.setPosition([0,-10,-1])
+view.setEuler([90,0,0])
 
+vizact.onkeydown(' ',view.setPosition([0,-10,-1]))
 
 sphere.disable(viz.PICKING)
 
@@ -27,23 +35,44 @@ visitedNozzles = False
 visitedShower = False
 visitedFireExtinguisher = False
 
+#creates equipment orbs
 fume = viz.addChild('white_ball.wrl')
 eye_wash = viz.addChild('white_ball.wrl')
-back = viz.addChild('white_ball.wrl')
+nozzles = viz.addChild('white_ball.wrl')
 shower = viz.addChild('white_ball.wrl')
 fire_ext = viz.addChild('white_ball.wrl')
 
+#Sets orbs to red
+fume.color( viz.RED )
+eye_wash.color( viz.RED )
+nozzles.color( viz.RED )
+shower.color( viz.RED )
+fire_ext.color( viz.RED )
+
+#positions orbs
 fume.setPosition([-2,-10,-13])
 eye_wash.setPosition([0,-12,-10])
-back.setPosition([3,-13,-7])
+nozzles.setPosition([3,-13,-7])
 shower.setPosition([8.5,-7,13])
 fire_ext.setPosition([-5,-10,15])
 
-textScreen = viz.addText('Screen Text',viz.SCREEN)
-textScreen.alignment(viz.ALIGN_RIGHT_BOTTOM)
-textScreen.setPosition([0.95,0.05,0])
+#Sets up screen 
+screen = vizshape.addPlane(size=(5,5), axis=vizshape.AXIS_X, cullFace=True)
+screen.setEuler(-180,0,0)
+screen.setPosition(15,-8,-2)
+screen.disable(viz.LIGHTING)
+screen.visible(viz.ON)
+screen.texture(viz.addTexture("Slides/Intro.jpg"))
 
-textScreen.message('')
+
+
+
+
+# Action for hiding/showing text\\
+DelayHide = vizact.sequence( vizact.waittime(10), vizact.method.visible(False) )
+Show = vizact.method.visible(True)\
+
+
 
 def updateScreenText():
 	global visitedFumeHood
@@ -53,22 +82,36 @@ def updateScreenText():
 	global visitedFireExtinguisher
 	object = viz.pick()
 	if object == fume: 
-		textScreen.message('FUME HOOD')
+		screen.texture(viz.addTexture("Slides/Fume_Hood.jpg"))
 		visitedFumeHood = True
+		fume.color( viz.GREEN )
+		view.setPosition([0,-10,-1])
+		view.setEuler([90,0,0])
 	elif object == eye_wash:
-		textScreen.message('EYE WASH STATION')
+		screen.texture(viz.addTexture("Slides/Eye_Wash.jpg"))
 		visitedEyeWash = True
-	elif object == back:
-		textScreen.message('OUTLETS & NOZZLES')
+		eye_wash.color( viz.GREEN )
+		view.setPosition([0,-10,-1])
+		view.setEuler([90,0,0])
+	elif object == nozzles:
+		screen.texture(viz.addTexture("Slides/Nozzles.jpg"))
 		visitedNozzles = True
+		nozzles.color( viz.GREEN )
+		view.setPosition([0,-10,-1])
+		view.setEuler([90,0,0])
 	elif object == shower:
-		textScreen.message('SAFETY SHOWER')
+		screen.texture(viz.addTexture("Slides/Shower.jpg"))
 		visitedShower = True
+		shower.color( viz.GREEN )
+		view.setPosition([0,-10,-1])
+		view.setEuler([90,0,0])
 	elif object == fire_ext:
-		textScreen.message('FIRE EXTINGUISHER')
 		visitedFireExtinguisher = True 
-	else:
-		textScreen.message('')
+		screen.texture(viz.addTexture("Slides/Fire_Extinguisher.jpg"))
+		fire_ext.color( viz.GREEN )
+		view.setPosition([0,-10,-1])
+		view.setEuler([90,0,0])
+		
 		
 	
 def tourOutput():
